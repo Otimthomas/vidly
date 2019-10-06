dc13e058573c9b86f708496268f11f18aadda968
otimthomas <totim444@hotmail.co.uk>
2019-10-06 14:07:28 +0300

    Added an up and down icon to the table header to show which column is sorted

--- src/components/common/moviesHeader.jsx
+++ src/components/common/moviesHeader.jsx
@@ -1,4 +1,9 @@
 import React, { Component } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+	faSortAlphaDown,
+	faSortAlphaUp
+} from "@fortawesome/free-solid-svg-icons";
 
 class MoviesHeader extends Component {
 	raiseSort = (path) => {
@@ -11,13 +16,26 @@ class MoviesHeader extends Component {
 		}
 		this.props.onSort(sortColumn);
 	};
+
+	renderSortIcon = (column) => {
+		const { sortColumn } = this.props;
+		if (sortColumn.path !== column.path) return null;
+		if (sortColumn.order === "asc")
+			return <FontAwesomeIcon icon={faSortAlphaDown} />;
+		return <FontAwesomeIcon icon={faSortAlphaUp} />;
+	};
+
 	render() {
 		const { columns } = this.props;
 		return (
 			<thead>
 				<tr>
 					{columns.map((column) => (
-						<th key={column.path || column.key} onClick={() => this.raiseSort(column.path)}>{column.label}</th>
+						<th
+							key={column.path || column.key}
+							onClick={() => this.raiseSort(column.path)}>
+							{column.label} {this.renderSortIcon(column)}
+						</th>
 					))}
 				</tr>
 			</thead>
